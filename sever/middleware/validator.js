77e008a6c5f36895056e0c0985ed79f86708fcf2
SuperRPM <vidafirmar@gmail.com>
2021-08-14 02:21:40 +0900

    update validator

--- sever/middleware/validator.js
+++ sever/middleware/validator.js
@@ -1,9 +1,18 @@
 import { validationResult } from 'express-validator';
 
-export const validate = (req, res, next) => {
-    if (tweet) {
-        res.status(200).json(tweet);
-    } else {
-        res.status(404).json({message: `Tweet id(${id}) is not exist`});
+// export const validate = (req, res, next) => {
+//     if (tweet) {
+//         res.status(200).json(tweet);
+//     } else {
+//         res.status(404).json({message: `Tweet id(${id}) is not exist`});
+//     }
+// };
+
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({message: errors.array()[0].msg });
     }
+    return next();
 };

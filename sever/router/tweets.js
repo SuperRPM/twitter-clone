3cf8a84a0181b4a96f7650629c079061249ded59
SuperRPM <vidafirmar@gmail.com>
2021-08-14 02:22:04 +0900

    apply validator

--- sever/router/tweets.js
+++ sever/router/tweets.js
@@ -2,35 +2,32 @@ import express from 'express';
 import 'express-async-errors';
 import * as tweetController from '../controller/tweet.js'
 // import { getAllTweetsById } from '../data/tweet.js';
-
+import {validate} from '../middleware/validator';
 const router = express.Router();
 
-const validate = (req, res, next) => {
-    if (tweet) {
-        res.status(200).json(tweet);
-    } else {
-        res.status(404).json({message: `Tweet id(${id}) is not exist`});
-    }
-};
-
-// GET /tweets
-// GET /tweets?username=:username
-router.get('/', validate, tweetController.getTweets);
-
-// GET /tweets/:id
-router.get('/:id', validate, tweetController.getTweet);
-
-// POST /tweets
-router.post('/', 
-[
+const validateTweet = [
     body('text')
         .isLength({ min: 3, max: 255 })
         .withMessage('너무 짧으면 안돼. 하지만 너무 길어도 안되지롱'),
-],
+        validate
+];
+
+// GET /tweets
+// GET /tweets?username=:username
+router.get('/', tweetController.getTweets);
+
+// GET /tweets/:id
+router.get('/:id', tweetController.getTweet);
+
+// POST /tweets
+router.post('/', 
+validateTweet,
 tweetController.postTweet);
 
 // PUT /tweets/:id
-router.put('/:id', validate, tweetController.updateTweet);
+router.put('/:id', 
+validateTweet, 
+tweetController.updateTweet);
 
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.removeTweet);

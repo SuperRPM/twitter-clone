cdcf56e525e710abf9c5cad1174db0dbb8efaf00
SuperRPM <vidafirmar@gmail.com>
2021-08-14 22:59:48 +0900

    add auth router APIs

--- sever/router/auth.js
+++ sever/router/auth.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import 'express-async-errors';
 import { body } from 'express-validator';
-import * as tweetController from '../controller/tweet.js';
 import * as authController from '../controller/auth.js';
 import { validate } from '../middleware/validator.js';
 
+// POST /auth/signup
+router.get('/signup', authController.signup);
+
+// POST/auth/login
+router.get('/login', authController.login);
+
+// POST /auth/me
+router.post('/me', authController.me);
+
+export default router;

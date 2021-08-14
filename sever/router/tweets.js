b2991ec50dfedd29f079ca7b93079f84c0c0d1d1
SuperRPM <vidafirmar@gmail.com>
2021-08-14 22:22:05 +0900

    apply validator to router

--- sever/router/tweets.js
+++ sever/router/tweets.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import 'express-async-errors';
+import { body } from 'express-validator';
 import * as tweetController from '../controller/tweet.js'
 // import { getAllTweetsById } from '../data/tweet.js';
-import {validate} from '../middleware/validator';
+import {validate} from '../middleware/validator.js';
+
+// 추가적으로 공부할것.
+// validation
+// sanitization
+// contract Testing: Client-Server
+
 const router = express.Router();
 
 const validateTweet = [
     body('text')
         .isLength({ min: 3, max: 255 })
         .withMessage('너무 짧으면 안돼. 하지만 너무 길어도 안되지롱'),
-        validate
+    validate
 ];
 
 // GET /tweets

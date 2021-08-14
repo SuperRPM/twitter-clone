c3220f3d38fa67f0cfbc777025f4755d06a6aeae
SuperRPM <vidafirmar@gmail.com>
2021-08-15 01:40:06 +0900

    update auth controller

--- sever/controller/auth.js
+++ sever/controller/auth.js
@@ -3,7 +3,6 @@ import * as userDatabase from '../data/auth.js';
 export async function signup(req, res) {
     const { username, name, email, photo, password } = req.body;
     const userData = [username, password];
-    console.log(userData);
     const data = await (userDatabase.getHashed(userData));
     res.status(200).json(data);
 }

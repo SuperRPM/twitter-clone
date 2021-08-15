93d11f6ff0c0606d800486136d3d0231e77a4885
SuperRPM <vidafirmar@gmail.com>
2021-08-15 14:48:03 +0900

    add validateSignup in auth router

--- sever/router/auth.js
+++ sever/router/auth.js
@@ -6,13 +6,21 @@ import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
 
+const validateSignup = [
+    ...validateCredential,
+    body('name').notEmpty().withMessage('이름이 없는뒈?'),
+    body('email').isEmail().normalizeEmail().withMessage('이메일 제대로 입력한거 맞아?'),
+    body('url'),isURL().withMessage.('경로가 이상한데?').optional({ nullable: true, checkFalsy: true }),
+    vlaidate,
+];
+
 // POST /auth/signup
-router.post('/signup', authController.signup);
+router.post('/signup', validateSignup, authController.signup);
 
 // POST/auth/login
-router.post('/login', authController.login);
+router.post('/login', validateCredentail, authController.login);
 
 // POST /auth/me
-router.post('/me', authController.me);
+// router.post('/me', authController.me);
 
 export default router;

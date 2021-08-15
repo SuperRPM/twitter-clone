eb3827eebb0282a9c3116826e43530b44a797897
SuperRPM <vidafirmar@gmail.com>
2021-08-15 22:27:49 +0900

    I didn't change anything but, git status tell me this is modified I have compared to remote version. but, still dosen't changed anything I have should surrenderd "git status" in the future, not to far. I will be come back. and vengence will be done!

--- sever/controller/tweet.js
+++ sever/controller/tweet.js
@@ -5,6 +5,7 @@ export async function getTweets(req, res) {
     const data = await (username 
         ? tweetRepository.getAllTweetsByUsername(username)
         : tweetRepository.getAllTweets());
+    console.log(data);
     res.status(200).json(data);
 }
 

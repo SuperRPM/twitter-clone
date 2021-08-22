f46d65941f496810b85ce4fbe4800a7d4fc0de5f
SuperRPM <vidafirmar@gmail.com>
2021-08-23 01:38:50 +0900

    sequelize

--- sever/data/tweet.js
+++ sever/data/tweet.js
@@ -1,33 +1,59 @@
-import { db } from '../db/database.js';
+import { Tweet, User } from '../db/database.js';
+import pkg from 'sequelize'
+const { Sequelize } = pkg;
+
+const INCLUDE_USER = {
+    attributes: ['id', 
+            'text', 
+            'createdAt', 
+            'userId', 
+            [Sequelize.col('user.name'), 'name'],
+            [Sequelize.col('user.username'), 'username'],
+            [Sequelize.col('user.url'), 'url'],
+        ],
+        include: {
+            model: User,
+            attributes: [],
+        },
+};
+
+const ORDER_DESC = { order: [['createdAt', 'DESC']] };
+
 
 export async function getAllTweets() {
-    return db.execute('SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.username, us.name, us.url FROM tweets as tw JOIN users as us ON tw.userId=us.id ORDER BY tw.createdAt DESC'
-    ).then(result => result[0]);
+    return Tweet.findAll({ ...INCLUDE_USER, ...ORDER_DESC });
 }
 
 export async function getAllTweetsByUsername(username) {
-    return db.execute('SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.username, us.name, us.url FROM tweets as tw JOIN users as us ON tw.userId=us.id WHERE username=? ORDER BY tw.createdAt DESC', [username])
-    .then((result) => result[0]);
+    return Tweet.findAll({ 
+        ...INCLUDE_USER, 
+        ...ORDER_DESC,
+        include: {
+            ...INCLUDE_USER.include,
+            where: {username: username},
+        },
+    });
 }
-
+//이게 시간이 지나니까 user테이블의 id인지 tweets테이블의 id인지 헷갈린다. 당연히 data/tweet이니까 tweet이겠지만 그래도 구분할 방법을 찾자.
 export async function getAllTweetsById(id) {
-    return db.execute('SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.username, us.name, us.url FROM tweets as tw JOIN users as us ON tw.userId=us.id WHERE tw.id=?', [id])
-    .then(result => result[0][0]);
+    return Tweet.findByPk(id, INCLUDE_USER)
 }
 
 export async function createTweet(text, userId) {
-    const createdAt = new Date();
-    return db.execute('INSERT INTO tweets (text, createdAt, userId) VALUES (?,?,?)', [text, createdAt, userId])
-    .then(result => getAllTweetsById(result[0].insertId));
+    return Tweet.create({ text, userId }).then(data => this.getAllTweetsById(data.dataValues.id));
 }
 
 export async function updateTweet(id, text) {
-    return db.execute('UPDATE tweets SET text=? WHERE id=?', [text, id])
-    .then(result => getAllTweetsById(id));
+    return Tweet.findByPk(id, INCLUDE_USER)
+    .then((tweet) => {
+        tweet.text = text;
+        return tweet.save();
+    })
 }
 
 export async function deleteTweet(id) {
-    return db.execute('DELETE FROM tweets WHERE id=?', [id])
-    // const indexToDelete = tweets.findIndex(function(tweet) {return tweet.id === id});
-    // if (indexToDelete >= 0) {tweets.splice(indexToDelete, 1)}
+    return Tweet.findByPk(id)
+    .then((tweet) => {
+        tweet.destroy();
+    })
 }
--- sever/db/database.js
+++ sever/db/database.js
@@ -1,11 +1,55 @@
-import mysql from 'mysql2';
+import pkg from 'sequelize';
 import { config } from '../config.js'
 
-const pool = mysql.createPool({
-    host: config.db.host,
-    user: config.db.user,
-    database: config.db.database,
-    password: config.db.password,
-})
+const { Sequelize, DataTypes } = pkg;
+const { host, user, database, password } = config.db;
+export const sequelize = new Sequelize(database, user, password, {
+    host,
+    dialect: 'mysql',
+    logging: false,
+});
 
-export const db = pool.promise();
+export const User = sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+    },
+    username: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING(128),
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+    },
+    url: {
+        type: DataTypes.TEXT,
+    }
+}, { timestamps: false });
+
+export const Tweet = sequelize.define('tweet', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+    },
+    text: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+}, { updatedAt: false });
+Tweet.belongsTo(User)//, {foreignKey: 'userId'})
